081e70615e0511ff7bf85d003461dd68f607f86e
amadeusantos <amadeusantos31@gmail.com>
2023-03-02 11:51:29 -0300

    configurando o backend

---
+++ adocao-api/src/main/java/com/adocaoapi/adocao/AdocaoApplication.java
@@ -0,0 +1,13 @@
+package com.adocaoapi.adocao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdocaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdocaoApplication.class, args);
+	}
+
+}
---
+++ adocao-api/src/test/java/com/adocaoapi/adocao/AdocaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adocaoapi.adocao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdocaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
