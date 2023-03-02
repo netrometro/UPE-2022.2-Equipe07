2e1d1228cbae19295f369d3c87956ca8843b435e
amadeusantos <amadeusantos31@gmail.com>
2023-03-02 18:23:38 -0300

    criando o modelo da entidade 'Usuario'

---
+++ adocao-api/src/main/java/com/adocaoapi/adocao/negocio/modelo/Usuario.java
@@ -0,0 +1,31 @@
+package com.adocaoapi.adocao.negocio.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String descricao;
+
+    private String email;
+
+    private String senha;
+
+}
