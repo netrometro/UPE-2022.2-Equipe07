550b82f2b74541679f45e6cbb7b114a6b332924b
amadeusantos <amadeusantos31@gmail.com>
2023-03-04 15:38:07 -0300

    configurando as rotas de usuario

--- adocao-api/src/rotas.ts
+++ adocao-api/src/rotas.ts
@@ -1,6 +1,13 @@
 import { Router } from "express";
+import { UsuarioCasosDeUso } from "./casosDeUso/usuario/UsuarioCasosDeUso";
+import { UsuarioControle } from "./casosDeUso/usuario/UsuarioControle";
 
 const rotas = Router();
+
+const usuarioCasosDeUso = new UsuarioCasosDeUso();
+const usuarioControle = new UsuarioControle();
+
+rotas.post("/usuarios/cadastro", usuarioControle.cadastrarUsuario);
 
 
 export{ rotas };
