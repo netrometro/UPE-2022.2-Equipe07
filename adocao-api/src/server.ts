9b40b29d61627247a97f94a6d3bf9cb4ddd66362
toni <antoniojsereno@gmail.com>
2023-03-04 14:45:56 -0300

    Hello World com Express

--- adocao-api/src/server.ts
+++ adocao-api/src/server.ts
@@ -1,1 +1,14 @@
 import express from "express";
+
+const app = express();
+
+app.listen(3333, () => {
+        console.log("### O HTTP Servidor esta Rodando Normalmente!!!")
+    }
+);
+
+app.get('/perfilanimal', 
+    (req, res)=>{
+        return res.send("Hello World!")
+    }
+);
