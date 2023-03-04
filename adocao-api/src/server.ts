3084c43188084f32ddba75c210b26c52d9a39ebc
toni <antoniojsereno@gmail.com>
2023-03-04 16:36:30 -0300

    Organização de rodas básicas GET e POST
    Organizando as rotas que o processo de acesso a perfil do animal e de cadastramento de novo animal iram utilizar

--- adocao-api/src/server.ts
+++ adocao-api/src/server.ts
@@ -2,13 +2,32 @@ import express from "express";
 
 const app = express();
 
+//funcao pra saber quando o servidor upou certinho e pam
 app.listen(3333, () => {
         console.log("### O HTTP Servidor esta Rodando Normalmente!!!")
     }
 );
 
+//Acesso ao perfil do animal
 app.get('/perfilanimal', 
     (req, res)=>{
-        return res.send("Hello World!")
+        return res.send("Aqui tem acesso ao perfil do animal");
     }
 );
+
+
+//->Cadastro de um novo animal
+//acesso ao formulario
+app.get('/animal/new', 
+    (req,res) => {
+        return res.send("Aqui tem acesso Formulario do novo animal");
+    }
+);
+
+//post do formulario
+app.post('/animal/new/processando',
+    (req,res) =>{
+        return res.send("Aqui ocorre o processo de cadastro e atualizaçao dos objetos e tabelas do BD")
+    }
+);
+
