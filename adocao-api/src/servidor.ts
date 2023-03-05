a13fc4209ef6ab9a6c25d764137979ae1f226663
higoru <104984808+higoru@users.noreply.github.com>
2023-03-04 22:58:02 -0300

    Revert "criando migrate no prisma"
    This reverts commit 6e9b48a96e1dde8ead8df48dc82eea154a930714.

--- adocao-api/src/servidor.ts
+++ adocao-api/src/servidor.ts
@@ -1,14 +1,10 @@
 import express from "express";
-import { rotas } from "./rotas/rotas";
+import { rotas } from "./rotas";
 
 const app = express();
 
 app.use(express.json());
 
 app.use(rotas);
-
-app.get('/', (req, res) => {
-    res.send('hello world');
-})
 
 app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
