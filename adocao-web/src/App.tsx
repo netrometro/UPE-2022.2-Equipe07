0b42eb43afcab944b5499981e365a965379f93d9
toni <antoniojsereno@gmail.com>
2023-03-05 11:10:23 -0300

    Update App.tsx com novas Rotas

--- adocao-web/src/App.tsx
+++ adocao-web/src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Post from './Pages/Post';
+import Show from './Pages/Show';
+import Edit from './Pages/Edit';
 
 function App() {
   return (
     <Router>
-      <Route path='/post' element={<Post />}/>
+      <Route path='/post' element={<Post />} />
+      <Route path='/show' element={<Show />} />
+      <Route path='/Edit' element={<Edit />} />
     </Router>
   );
 }
---
+++ adocao-web/src/Pages/Show.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Show = () => {
+    return (
+        <>
+        <h1>Page Post</h1>
+        </>
+    );
+};
+
+export default Show;
