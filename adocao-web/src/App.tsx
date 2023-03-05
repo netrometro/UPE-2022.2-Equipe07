1e58963d68c4d211806d966709bb11b35c9a1e39
toni <antoniojsereno@gmail.com>
2023-03-05 11:06:19 -0300

    Configurando rotas no App.tsx com Route

--- adocao-web/src/App.tsx
+++ adocao-web/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Post from './Pages/Post';
 
 function App() {
   return (
     <Router>
-      <Route />
+      <Route path='/post' element={<Post />}/>
     </Router>
   );
 }
