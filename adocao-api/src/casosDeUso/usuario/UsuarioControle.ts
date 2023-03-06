677b18d4c1089db38ce38b6b1d9184a09a7c318c
amadeusantos <amadeusantos31@gmail.com>
2023-03-05 23:01:49 -0300

    fazendo o controle de atualização de senha

--- adocao-api/src/casosDeUso/usuario/UsuarioControle.ts
+++ adocao-api/src/casosDeUso/usuario/UsuarioControle.ts
@@ -17,4 +17,16 @@ export class UsuarioControle {
 
         return response.status(201).json();
     }
+
+    async atualizarSenha(request: Request, response: Response) {
+        const { novaSenha } = request.body;
+        const autorToken = request.headers.authorization;
+        if (!autorToken) {throw new Error("Sem token!")};
+        const [ ,token] = autorToken.split(" ");
+
+        const usuarioCasosDeUso = new UsuarioCasosDeUso();
+        await usuarioCasosDeUso.atualizarSenha({ token, novaSenha });
+
+        return response.status(200).json();
+    }
 }
