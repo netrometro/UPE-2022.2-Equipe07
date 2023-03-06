7eddd7a60480a7ed4e0ad7658797318271acb48c
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-03-05 21:52:38 -0300

    atualizando server/index para postagem
    configuração do server

--- adocao-api/src/servidor.ts
+++ adocao-api/src/servidor.ts
@@ -1,14 +1,31 @@
 import express from "express";
 import { rotas } from "./rotas/rotas";
+import bodyParser from 'body-parser';
+const cors = require('cors');
+const morgan = require('morgan');
+import jwt from 'jsonwebtoken';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(rotas);
 
+app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
+
+const postRoutes = require('./rotas/postagemRotas');
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('dev'));
+
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.send('Hello World!')
 })
 
-app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
+app.use('/api/posts', postRoutes);
