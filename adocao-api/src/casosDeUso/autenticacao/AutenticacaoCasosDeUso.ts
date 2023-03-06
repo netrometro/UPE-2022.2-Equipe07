3fc442ef64c2d8cdc2d4f316ae1e7ec648caac32
amadeusantos <amadeusantos31@gmail.com>
2023-03-05 21:39:10 -0300

    fazendo os casos de uso da recuperacao de conta

--- adocao-api/src/casosDeUso/autenticacao/AutenticacaoCasosDeUso.ts
+++ adocao-api/src/casosDeUso/autenticacao/AutenticacaoCasosDeUso.ts
@@ -1,13 +1,15 @@
 import { compare } from "bcryptjs";
+import { verify } from "jsonwebtoken";
 import { client } from "../../prisma/client";
-import { GeradorDeTokenProvedor } from "../../providores/geradorDeToken";
+import { EnviarEmail } from "../../provedores/envioDeEmail";
+import { GeradorDeTokenProvedor } from "../../provedores/geradorDeToken";
 
 interface AutenticacaoUsuario {
     email: string;
     senha: string;
 }
 export class AutenticacaoCasosDeUso {
-    async autenticarUsuario({email, senha}: AutenticacaoUsuario) {
+    async autenticarUsuario({ email, senha }: AutenticacaoUsuario) {
         const usuarioExistente = await client.usuario.findFirst({
             where: {
                 email
@@ -28,4 +30,72 @@ export class AutenticacaoCasosDeUso {
         const token = await geradorDeTokenProvedor.gerar(usuarioExistente.id);
         return { token };
     }
+
+    async recuperacaoDeConta({ email }: RecuperacaoDeContaProps) {
+        const usuarioExistente = await client.usuario.findFirst({
+            where: {
+                email
+            }
+        });
+
+        if (!usuarioExistente) {
+            throw new Error("Usuário existente!");
+        }
+
+        const geradorDeToken = new GeradorDeTokenProvedor();
+        const tokenDeRecuperacao = await geradorDeToken.gerar(usuarioExistente.id);
+
+
+        await client.recuperacao.create({
+            data: {
+                usuarioId: usuarioExistente.id,
+                token: tokenDeRecuperacao
+            }
+        })
+
+
+        const enviarEmail = new EnviarEmail();
+        await enviarEmail.enviar({
+            subject: 'Recuperação de conta no adocão!',
+            nome: usuarioExistente.nome,
+            email,
+            corpo: [
+                '<div style="font-famaly: sans-serif; font-size: 16px; color: #111;">',
+                `<h1>Código de recuperação: ${tokenDeRecuperacao}</h1>`,
+                `<p></p>`,
+                '</div>'
+            ].join('\n')
+        })
+    }
+
+    async verificarTokenDeRecuperacao({ tokenDeRecuperacao }: VerificarTokenDeRecuperacaoProps) {
+        try {
+            const decoded = await verify(tokenDeRecuperacao, "aa0aae8f-505d-408f-83a5-c71cebc1349b");
+            const geradorDeToken = new GeradorDeTokenProvedor();
+            const token = await geradorDeToken.gerar(decoded.toString());
+            client.recuperacao.update({
+                where: {
+                    id: decoded.toString()
+                },
+                data: {
+                    token: null
+                }
+            });
+            
+            return { token };
+        } catch (err) {
+            throw new Error("Token inválido!");
+        };
+
+
+    }
+
+}
+interface RecuperacaoDeContaProps {
+    email: string;
+}
+
+
+interface VerificarTokenDeRecuperacaoProps {
+    tokenDeRecuperacao: string;
 }
