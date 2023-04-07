2e177974dee73c5ca72d2fa2f3180cc982e9eab7
Amadeu Santos <amadeusantos31@gmail.com>
2023-04-07 15:31:35 -0300

    fazendo o controle de filtro de Dica

--- adocao-api/src/casosDeUso/dica/DicaControle.ts
+++ adocao-api/src/casosDeUso/dica/DicaControle.ts
@@ -1,10 +1,19 @@
 import { DicaCasosDeUso } from "./DicaCasosDeUso";
+import { Request, Response } from "express";
 
 class DicaControle {
     async listarDicas(request: Request, response: Response) {
         const dicaCasosDeUso = new DicaCasosDeUso();
         const dicas = await dicaCasosDeUso.listarDicas();
         
-        return dicas;
+        return response.json(dicas);
+    }
+
+    async filtrarDicasCategoria(request: Request, response: Response) {
+        const { categoria } = request.params;
+        const dicaCasosDeUso = new DicaCasosDeUso();
+        const dicas = await dicaCasosDeUso.filtarDicasCategoria(categoria);
+
+        return response.json(dicas);
     }
 }
