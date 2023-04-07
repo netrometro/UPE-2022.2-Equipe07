8cfbe6c7ddcba1115e14098bd0bb360852793602
Amadeu Santos <amadeusantos31@gmail.com>
2023-04-07 15:31:08 -0300

    fazendo caso de uso de filtro de Dica

--- adocao-api/src/casosDeUso/dica/DicaCasosDeUso.ts
+++ adocao-api/src/casosDeUso/dica/DicaCasosDeUso.ts
@@ -1,8 +1,30 @@
+import { CategoriaEnum } from "@prisma/client";
 import { client } from "../../prisma/client";
 
 export class DicaCasosDeUso {
     async listarDicas() {
         const dicas = await client.dica.findMany();
+
+        return dicas;
+    }
+
+    async filtarDicasCategoria(categoria: string) {
+        var categoriaEnum;
+        switch (categoria) {
+        case "CUIDADO":
+            categoriaEnum = CategoriaEnum.CUIDADO
+        case "EXERCICIO":
+            categoriaEnum = CategoriaEnum.EXERCICIO
+        case "NUTRICAO":
+            categoriaEnum = CategoriaEnum.NUTRICAO
+        case "TREINAMENTO":
+            categoriaEnum = CategoriaEnum.TREINAMENTO
+    }
+        const dicas = await client.dica.findMany({
+            where: {
+                categoria: categoriaEnum
+            }
+        });
 
         return dicas;
     }
