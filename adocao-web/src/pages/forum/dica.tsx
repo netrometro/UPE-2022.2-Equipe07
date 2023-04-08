da74fe485373533b719ef20d4cd72a1c5d8cae99
Amadeu Santos <amadeusantos31@gmail.com>
2023-04-08 02:04:12 -0300

    conectando com o backend do Dica

--- adocao-web/src/pages/forum/dica.tsx
+++ adocao-web/src/pages/forum/dica.tsx
@@ -1,47 +1,64 @@
 import { useParams } from "react-router-dom";
 import { CabecalhoHome } from "../../componentes/cabecalhoHome";
 import { DicaForum } from "../../componentes/dicaForum";
+import { useEffect, useState } from "react";
+import { api } from "../../lib/api";
 
-interface DicaProps {
-    dica: {
-    id: number,
-	imgURL: string;
-	titulo: string;
-	autor: string;
-	criacao: string;
-	texto: string;
-	categoria: string;
+ 
+interface Comentarios {
+    id: number;
+    texto: string;
+    usuario: {
+        nomeDeUsuario: string;
     }
-    
 }
 
-    const dica = {
-		id: 1,
-		imgURL: "https://fisioanimal.com/blog/wp-content/uploads/2019/06/shutterstock_474506101-752x470.jpg",
-		titulo: "Preparação para receber cachorro filhote",
-		autor: "FISIOANIMAL",
-		criacao: "2023-04-01T16:23:01.589Z",
-		texto: "Preparar a casa para receber o novo amigo irá facilitar a sua vida e dar mais conforto ao animal. Comece escolhendo um canto tranquilo para colocar a caminha de cachorro — é importante que seja uma local silencioso e sem muita movimentação, pois nos primeiros dias é normal que o cão fique inseguro. Além disso, no primeiro mês de vida o cão costuma dormir bastante e, para isso, precisa contar com um ambiente calmo. Além da cama, você deve providenciar bebedouro e comedouro para cachorro. Também é fundamental pensar na alimentação canina antes mesmo que ele chegue à sua casa. Tenha o cuidado de escolher o alimento ideal para a idade do animal.",
-		categoria: "CUIDADO"
-	}
-
 export function Dica() {
-    const params = useParams();
-    const id = Number.parseInt(params.id!);
-    return(
-        <div>
-            <CabecalhoHome />
-        <div className=" items-center bg-[url('./assets/bg.jpg')] bg-no-repeat bg-center bg-cover">
-            <section className="bg-slate-100/[0.6] p-10 rounded-xl mx-20 m-2.5">
-            <DicaForum dica={dica} />
-            <div className="bg-slate-100/[0.6] p-5 rounded-xl mx-20 m-2.5">
-                <section className="my-2">
-                    <h3 className="font-extrabold">docas</h3>
-                    <p>mpedit!</p>
-                </section>
-            </div>
+  const params = useParams();
+  const id = Number.parseInt(params.id!);
+  const [iid, setId] = useState(0);
+  const [titulo, setTitulo] = useState("");
+  const [imgURL, setImgURL] = useState("");
+  const [autor, setAutor] = useState("");
+  const [criacao, setCriacao] = useState("");
+  const [texto, setTexto] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [comentarios, setComentarios] = useState([]);
+
+
+  useEffect(() => {
+    api.get("/dica/" + params.id).then((res) => {
+
+        setId(res.data.id)
+        setTitulo(res.data.titulo)
+        setImgURL(res.data.imgURL)
+        setAutor(res.data.autor)
+        setCriacao(res.data.criacao)
+        setTexto(res.data.texto)
+        setCategoria(res.data.categoria)
+    });
+
+    api.get("/comentarios/dicas/" + id).then((res) => setComentarios(res.data));
+  }, []);
+
+  return (
+    <div>
+      <CabecalhoHome />
+      <div className=" items-center bg-[url('./assets/bg.jpg')] bg-no-repeat bg-center bg-cover">
+        <section className="bg-slate-100/[0.6] p-10 rounded-xl mx-20 m-2.5">
+          <DicaForum dica={{id, titulo, imgURL, autor, criacao, texto, categoria}} />
+          <div className="bg-slate-100/[0.6] p-5 rounded-xl mx-20 m-2.5">
+            {comentarios.map((com: Comentarios) => {
+                return (
+            <section className="my-2" key={com.id}>
+              <h3 className="font-extrabold">{com.usuario.nomeDeUsuario}</h3>
+              <p>{com.texto}</p>
             </section>
-        </div>
-        </div>
-    )
+                )
+            })}
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 }
